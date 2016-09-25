3db70518af47f20746b7ec38ae16cfdc4a2264a6
MJ <myoungjun.kim@gmail.com>
2016-09-26 01:31:32 +0900

    Initial Commit

---
+++ MJSON/MJSON.h
@@ -0,0 +1,19 @@
+//
+//  MJSON.h
+//  MJSON
+//
+//  Created by MJ Kim on 2016. 9. 26..
+//  Copyright © 2016년 Swifter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJSON.
+FOUNDATION_EXPORT double MJSONVersionNumber;
+
+//! Project version string for MJSON.
+FOUNDATION_EXPORT const unsigned char MJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJSON/PublicHeader.h>
+
+
